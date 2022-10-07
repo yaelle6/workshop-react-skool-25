7cd3e8c14ca6b64a708c83b8bdbc1ca6995b35fb
Olivia Peny-Coblentz <oliv@octo.com>
2022-10-07 12:09:32 +0200

    create empty component with first test

---
+++ src/routes/Home.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom'
+import Home from './Home';
+
+describe('Home Page', () => {
+  it('possède un titre de niveau h1', () => {
+    render(<Home />, {wrapper: BrowserRouter});
+    const titleElement = screen.getByRole('heading', {level: 1})
+    expect(titleElement).toBeInTheDocument();
+  })
+
+  it('possède un lien qui redirige vers la page articles', () => {
+    render(<Home />, {wrapper: BrowserRouter});
+    // ...
+  })
+});
